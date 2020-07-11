0492cb2f232c37e7a3a176adf2ad5d39fa1d79dc
s13ls <61311344+s13ls@users.noreply.github.com>
2020-07-11 15:04:32 +0700

    dasfF

--- js/script.js
+++ js/script.js
@@ -9,5 +9,19 @@ $(document).ready(function() {
             top = $(id).offset().top; //определим высоту от начала страницы до якоря
     $('body,html').animate({scrollTop: top}, 1000); //сделаем прокрутку за 1 с
     });
+
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 0) {
+            $('#scroller').fadeIn();
+        } else {
+            $('#scroller').fadeOut();
+        }
+    });
+    $('#scroller').click(function () {
+        $('body,html').animate({
+            scrollTop: 0
+        }, 200);
+        return false;
+    });
     
 });
